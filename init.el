c31e0e0a1e3e63ebad1863d7521431c9f315519d
Martin Borchert <martin@nt-5.com>
2020-10-10 15:13:33 +0200

    Setup recommended org keybindings

--- init.el
+++ init.el
@@ -236,6 +236,11 @@
 ;; Org
 ;; =====================================================================
 
+
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
 ;; org-journal
 (use-package org-journal
   :defer t
